bcc5fe7ff0a5168d14ad9a2d97299dec86c0ce37
Gaurav Gautam <gautam1168@gmail.com>
2025-07-01 21:52:04 +0530

    L3

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	words := strings.Split(text, " ")
+	result := make([]string, 0, len(words))
+	for _, word := range words {
+		trimmed := strings.Trim(word, " ")
+		if trimmed == "" {
+			continue
+		}
+
+		lowercased := strings.ToLower(trimmed)
+
+		result = append(result, lowercased)
+	}
+	return result
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	cleanInput("  hello  world  ")
 }
