dbaf64451b005447ade39a90b7804f77a1968f89
Gaurav Gautam <gautam1168@gmail.com>
2025-07-04 16:01:57 +0530

    L5

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(r cmdRegistry) error
+}
+
+type cmdRegistry = map[string]cliCommand
+
 func cleanInput(text string) []string {
 	words := strings.Split(text, " ")
 	result := make([]string, 0, len(words))
@@ -23,15 +31,49 @@ func cleanInput(text string) []string {
 	return result
 }
 
+func commandExit(r cmdRegistry) error {
+	fmt.Println("\nClosing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(r cmdRegistry) error {
+	fmt.Println("\nWelcome to the Pokedex!")
+	fmt.Printf("Usage: \n\n")
+	for _, cmd := range r {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
 func main() {
+
 	scanner := bufio.NewScanner(os.Stdin)
+
+	registry := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		inputLine := scanner.Text()
 		words := cleanInput(inputLine)
-		if len(words) > 0 {
-			fmt.Printf("Your command was: %s\n", words[0])
+		command := words[0]
+
+		if cmd, ok := registry[command]; ok {
+			cmd.callback(registry)
+		} else {
+			fmt.Println("Unknown command")
 		}
 	}
 }
